bf540d9dc4dd4c660b7467041b0c31a52cee0bd2
Tonzi254 <danteskilz@gmail.com>
2023-07-26 23:12:12 +0300

    deleted .env and _config.js files

--- _config.js
+++
@@ -1,9 +0,0 @@
-var config = {}
-
-// Update to have your correct username and password
-config.mongoURI = {
-    production: 'mongodb+srv://drotich:iEjHMgYNKU8vMFP2@jenkins.avjpxpk.mongodb.net/darkroom',
-    development: 'mongodb+srv://drotich:iEjHMgYNKU8vMFP2@jenkins.avjpxpk.mongodb.net/darkroom-dev',
-    test: 'mongodb+srv://drotich:iEjHMgYNKU8vMFP2@jenkins.avjpxpk.mongodb.net/darkroom-test',
-}
-module.exports = config;
